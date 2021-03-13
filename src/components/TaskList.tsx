556f2184d89d832eb73d50d818308421ae333821
Ronaldo Ferreyra <rferreyra@reweb.com.br>
2021-03-13 00:42:00 -0300

    Remover alerta

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -17,7 +17,6 @@ export function TaskList() {
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (newTaskTitle.length == 0) {
-      alert('Favor informar uma tarefa!');
       return false;
     }
 
